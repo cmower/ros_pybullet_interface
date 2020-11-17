6ff0bea93c5cf04ff67f799e6e0e74efebae5f92
Chris Mower <chris.mower@ed.ac.uk>
2020-11-17 18:19:48 +0000

    Major re-vamp, details below * config load moved to utils.py, dedicated script * pybullet interface code moved to dedicated script * Use DT rather than having it as an attribute of the interface class * Listen to tfs as well as publish them (allow user to attach an object to a tf frame) * Generally a cleaner interface * Setup publishers/subscribers in dedicated setup methods (apart from main pybullet update)

---
+++ include/utils.py
@@ -0,0 +1,10 @@
+import yaml
+
+def loadYAMLConfig(file_name):
+    with open(file_name, 'r') as configfile:
+         config = yaml.load(configfile, Loader=yaml.FullLoader)
+    return config
+
+__all__=[
+    'loadYAMLConfig'
+]
