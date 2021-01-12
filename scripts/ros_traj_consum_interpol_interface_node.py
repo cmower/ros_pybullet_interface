33c26809b0a2767da4ec5ab3d147fe46dedd8226
stoutheo <stoutheo@gmail.com>
2021-01-12 01:35:08 +0000

    Trajectory interpolation node up and running 	Pending: 		i)   Listening to new trajectories + general msg type 		ii)  Write a test example file 		iii) Shutting down gracefully - if all waypts are consumed 		iv)  Thread-safe read-delete-write 		v)   Synchronized consuming (maybe)

--- scripts/ros_traj_consum_interpol_interface_node.py
+++ scripts/ros_traj_consum_interpol_interface_node.py
@@ -27,28 +27,23 @@ END_EFFECTOR_TARGET_FRAME_ID = 'ros_pybullet_interface/end_effector/target'
 
 class TrajManager:
 
-    def __init__(self, hor_len, use_interpolation, inter_dt,\
-                    rob_traj_dim, def_rob_mot_axes, def_rob_mot_axes_val, \
-                    obj_traj_dim, def_obj_mot_dim, def_obj_mot_axes_val):
+    def __init__(self, mot_dim, interpol):
 
-        consumerCounter = 0
-        self.horizonLength = hor_len
-        self.interFreq = 1/inter_dt
-        self.use_interp = use_interpolation
 
-        self.rob_traj_dim = rob_traj_dim
-        self.obj_traj_dim = obj_traj_dim
-
-        self.def_rob_mot_axes_val = def_rob_mot_axes_val
-        self.def_obj_mot_axes_val = def_obj_mot_axes_val
+        self.horizonLength  = interpol['horizon_Length']
+        self.interFreq = 1.0/interpol['interDt']
+        self.use_interp = interpol['use_interpolation']
 
         # number of dimentions of the trajectory plan
-        trajPlanDim = len(rob_traj_dim) + len(obj_traj_dim)
+        trajPlanDim = mot_dim["number"]
+
+        # information about the fixed and planned dimensions of the motion
+        self.mot_dim = mot_dim
 
         # init struct for trajectory plan received by a planner
         self.trajPlan = np.zeros((trajPlanDim, 1))
 
-        if use_interpolation:
+        if self.use_interp:
             self.dTrajPlan = np.zeros((trajPlanDim, 1))
             self.time = np.zeros((1, 1))
 
@@ -65,32 +60,53 @@ class TrajManager:
     def transTraj2Motion6D(self, way_pt):
         """ We need a function that maps dimensions of the Traj to 6D"""
 
-        # Robot end-effector
-        pos_rob = np.array([way_pt[3], way_pt[4], self.def_rob_mot_axes_val[0]])
-        eeOri_Rot_rob = R.from_rotvec(self.def_rob_mot_axes_val[1:])
 
-        eeOri_Rot_rob = R.from_quat(np.array([1.0, 0.0, 0.0,  -0.0]))
-        # eeOri_Rot_rob = R.from_quat(np.array([0.991629064083, 0.00588622735813, 0.128958553076,  -0.0026147547178]))
-        eeOri_rob = eeOri_Rot_rob.as_quat()
+        # translation
+        if self.mot_dim['trans']['translationX'] is not None:
+            x = self.mot_dim['trans']['translationX']
+        else:
+            x = way_pt[self.mot_dim['trans']['translationX_index']]
 
-        # object
-        pos_obj = np.array([way_pt[0], way_pt[1], self.def_obj_mot_axes_val[0]])
+        if self.mot_dim['trans']['translationY'] is not None:
+            y = self.mot_dim['trans']['translationY']
+        else:
+            y = way_pt[self.mot_dim['trans']['translationY_index']]
 
-        # requires scipy 1.6.0 --- not available yet in my pip
-        # source : https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Rotation.from_mrp.html#scipy.spatial.transform.Rotation.from_mrp
-        # mrp_vec = np.array(self.def_obj_mot_axes_val[1:])*np.tan(way_pt[2]/4)
-        # eeOri_Rot_obj = R.from_mrp(mrp_vec)
 
-        rot_mat = utils.rotation_matrix_from_axis_angle(np.array(self.def_obj_mot_axes_val[1:]), way_pt[2])
-        eeOri_Rot_obj = R.from_matrix(rot_mat)
-        eeOri_obj = eeOri_Rot_obj.as_quat()
+        if self.mot_dim['trans']['translationZ'] is not None:
+            z = self.mot_dim['trans']['translationZ']
+        else:
+            z = way_pt[self.mot_dim['trans']['translationZ_index']]
 
-        return np.hstack((pos_rob,eeOri_rob)), np.hstack((pos_obj, eeOri_obj))
+        pos = np.array([x, y, z])
+
+        # rotation
+        if self.mot_dim['rotation']['rotationTheta'] == True:
+            rot_mat = utils.rotation_matrix_from_axis_angle(np.array(self.mot_dim['rotation']['rotationvec']), way_pt[self.mot_dim['rotation']['rotationTheta_index']])
+            Ori_Rot = R.from_matrix(rot_mat)
+
+        else:
+            if self.mot_dim['rotation']['rotationvec'] is not None:
+                Ori_Rot = R.from_rotvec(np.array(self.mot_dim['rotation']['rotationvec']))
+            else:
+                idx = self.mot_dim['rotation']['rotationvec_index']
+                Ori_Rot = R.from_quat(np.array(way_pt[idx[0]:idx[1]]))
+
+        # eeOri_Rot_rob = R.from_quat(np.array([1.0, 0.0, 0.0,  -0.0]))
+        Ori = Ori_Rot.as_quat()
+
+        return np.hstack((pos, Ori))
 
 
     # --------------------------------------------------------------------
     #   ATTENTION : popFirstTrajElem and updateTraj need to be thread safe!!!
     #             At the moment they are NOT!!!!!
+    #
+    #    READ more ...
+    #       maybe multithreading
+    #    maybe threading
+    # use lock:
+    # https://www.bogotobogo.com/python/Multithread/python_multithreading_Synchronization_Lock_Objects_Acquire_Release.php
     # --------------------------------------------------------------------
 
     def popFirstTrajElem(self):
@@ -121,9 +137,20 @@ class TrajManager:
             data = np.load(f)
 
         # update the trajectory plan#
-        self.trajPlan = np.hstack((self.trajPlan, np.vstack( (data[0:3,:], data[6:8,:]))))
-        self.dTrajPlan = np.hstack((self.dTrajPlan, np.vstack((data[3:6,:], data[8:10,:]))))
-        self.time = np.hstack((self.time, 0.0001 + data[10,:].reshape(1,data[10,:].shape[0])))
+        # self.trajPlan = np.hstack((self.trajPlan, np.vstack( (data[0:3,:], data[6:8,:]))))
+        # self.dTrajPlan = np.hstack((self.dTrajPlan, np.vstack((data[3:6,:], data[8:10,:]))))
+        # self.time = np.hstack((self.time, 0.0001 + data[10,:].reshape(1,data[10,:].shape[0])))
+
+        if self.horizonLength==20:
+            self.trajPlan = np.hstack((self.trajPlan,  data[6:8,:]))
+            self.dTrajPlan = np.hstack((self.dTrajPlan, data[8:10,:]))
+            self.time = np.hstack((self.time, 0.0001 + data[10,:].reshape(1,data[10,:].shape[0])))
+
+        if self.horizonLength==10:
+            self.trajPlan = np.hstack((self.trajPlan,  data[0:3,:]))
+            self.dTrajPlan = np.hstack((self.dTrajPlan, data[3:6,:]))
+            self.time = np.hstack((self.time, 0.0001 + data[10,:].reshape(1,data[10,:].shape[0])))
+
 
         if self.use_interp:
             self.motionInterpPlan = self.computeInterpTraj()
@@ -158,6 +185,10 @@ class ROSTrajInterface(object):
         # Setup constants
         self.dt = 1.0/float(FREQ)
 
+        # default values for TF
+        self.header_frame_id = None
+        self.msg_child_frame_id = None
+
         # Name of node
         self.name = rospy.get_name()
         # Initialization message
@@ -173,9 +204,7 @@ class ROSTrajInterface(object):
         self.setupTrajManager(traj_config_file_name)
 
         self.tfBroadcaster = tf2_ros.TransformBroadcaster()
-        rospy.sleep(5.0)
-
-        self.flag = 0
+        rospy.sleep(1.0)
 
 
     def setupTrajManager(self, config_file_name):
@@ -184,22 +213,14 @@ class ROSTrajInterface(object):
         config = utils.loadYAMLConfig(os.path.join(self.current_dir,config_file_name))
 
         # Extract data from configuration
-        rob_traj_dim = config['robot_trajectory_dimensions']
-        def_rob_mot_axes = config['default_robot_motion_axes']
-        def_rob_mot_axes_val = config['default_robot_motion_axes_values']
+        mot_dim = config['motion_dimensions']
 
-        if config['object_properties']:
-            obj_traj_dim = config['object_trajectory_dimensions']
-            def_obj_mot_dim = config['default_object_motion_dimensions']
-            def_obj_mot_axes_val = config['default_object_motion_axes_values']
-        else:
-            obj_traj_dim = None
-            def_obj_mot_dim = None
-            def_obj_mot_axes_val = None
+        # Extract data from configuration
+        interpol = config['interpolation']
 
-        hor_len = config['horizon_Length']
-        use_interp = config['use_interpolation']
-        interDt = config['interDt']
+        # set info about TF
+        self.header_frame_id = config['communication']['header_frame_id']
+        self.msg_child_frame_id = config['communication']['msg_child_frame_id']
 
         # Establish connection with planning node
         # rospy.loginfo("%s: Waiting for "+CURRENT_JOINT_STATE_TOPIC +" topic", self.name)
@@ -207,9 +228,7 @@ class ROSTrajInterface(object):
         # self.startListening2Trajectories(msgRobotState)
 
         # Create trajectory manager instance
-        self.trajManag = TrajManager(hor_len, use_interp, interDt,\
-                                    rob_traj_dim, def_rob_mot_axes, def_rob_mot_axes_val, \
-                                    obj_traj_dim, def_obj_mot_dim, def_obj_mot_axes_val)
+        self.trajManag = TrajManager(mot_dim, interpol)
 
         #  temp call to load trajectory data
         self.trajManag.updateTraj()
@@ -225,43 +244,24 @@ class ROSTrajInterface(object):
 
     def publishdNextWayPtToROS(self, event):
 
-            robot, obj = self.trajManag.getNextWayPt()
+            motion = self.trajManag.getNextWayPt()
 
             # Pack pose msg
             msg = TransformStamped()
             msg.header.stamp = rospy.Time.now()
-            msg.header.frame_id = 'ros_pybullet_interface/world'
-            msg.child_frame_id = 'ros_pybullet_interface/end_effector/target'
-            msg.transform.translation.x = robot[0]
-            msg.transform.translation.y = robot[1]
-            msg.transform.translation.z = robot[2]
-            msg.transform.rotation.x = robot[3]
-            msg.transform.rotation.y = robot[4]
-            msg.transform.rotation.z = robot[5]
-            msg.transform.rotation.w = robot[6] # NOTE: the ordering here may be wrong
+            msg.header.frame_id = self.header_frame_id
+            msg.child_frame_id = self.msg_child_frame_id
+            msg.transform.translation.x = motion[0]
+            msg.transform.translation.y = motion[1]
+            msg.transform.translation.z = motion[2]
+            msg.transform.rotation.x = motion[3]
+            msg.transform.rotation.y = motion[4]
+            msg.transform.rotation.z = motion[5]
+            msg.transform.rotation.w = motion[6] # NOTE: the ordering here may be wrong
 
             # Publish msg
             self.tfBroadcaster.sendTransform(msg)
 
-
-            # Pack pose msg
-            msg = TransformStamped()
-            msg.header.stamp = rospy.Time.now()
-            msg.header.frame_id = 'ros_pybullet_interface/world'
-            msg.child_frame_id = 'ros_pybullet_interface/box'
-            msg.transform.translation.x = obj[0]
-            msg.transform.translation.y = obj[1]
-            msg.transform.translation.z = obj[2]
-            msg.transform.rotation.x = obj[3]
-            msg.transform.rotation.y = obj[4]
-            msg.transform.rotation.z = obj[5]
-            msg.transform.rotation.w = obj[6] # NOTE: the ordering here may be wrong
-
-            # Publish msg
-            self.tfBroadcaster.sendTransform(msg)
-            if self.flag == 0:
-                rospy.sleep(2.0)
-                self.flag = 1
 
     def cleanShutdown(self):
         print('')
