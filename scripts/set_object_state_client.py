0e1de307b771af9e58698b726a1f09a2272d89e4
Lei <lei.yan@ed.ac.uk>
2021-04-09 15:34:05 +0100

    set new pose and velocity in the client file.

--- scripts/set_object_state_client.py
+++ scripts/set_object_state_client.py
@@ -5,15 +5,13 @@ import sys
 import rospy
 from ros_pybullet_interface.srv import setObjectState, setObjectStateResponse
 
-
-if __name__ == "__main__":
-
+def main():
     # set the state of the object
-    obj_name = "catch_box"
-    pos = [-1, 0, 0.1]
+    obj_name = "target"
+    pos = [0, -0.6, 0.30]
     quat = [0, 0, 0, 1]
-    lin_vel = [0.2, 0, 0]
-    ang_vel = [0, 0, 0]
+    lin_vel = [0.1, 0.0, 0.0]
+    ang_vel = [0.1, 0.1, 0.1]
     rospy.wait_for_service('set_object_state')
     try:
         set_object_state = rospy.ServiceProxy('set_object_state', setObjectState)
