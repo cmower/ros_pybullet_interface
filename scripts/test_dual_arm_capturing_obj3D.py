de67884b8b5358523fd86550d1c095a69265036e
iiwa-kuka <stoutheo@gmail.com>
2021-06-10 17:56:45 +0100

    Renaming and cleaning files

--- scripts/test_dual_arm_capturing_obj3D.py
+++ scripts/test_dual_arm_capturing_obj3D.py
@@ -22,8 +22,8 @@ from py_pack import yaml
 from py_pack import hybrid_tosrb
 from py_pack import hybrid_todac
 
-NEW_TRAJ_Yang_TOPIC = 'yang/ros_pybullet_interface/end_effector/traj' # publishes end-effector planned trajectory on this topic
-NEW_TRAJ_Yin_TOPIC = 'yin/ros_pybullet_interface/end_effector/traj' # publishes end-effector planned trajectory on this topic
+
+NEW_TRAJ_TOPIC= 'ros_pybullet_interface/end_effector/traj' # publishes end-effector planned trajectory on this topic
 NEW_TRAJ_OBJ_TOPIC = 'ros_pybullet_interface/object/traj' # publishes end-effector planned trajectory on this topic
 
 WORLD_FRAME_ID = 'ros_pybullet_interface/world'
@@ -38,6 +38,14 @@ class PlanInterpWithTO:
         # Name of node
         self.name = rospy.get_name()
 
+        # check if the name of the robot is provided
+        if rospy.has_param('~robot1_name') and rospy.has_param('~robot2_name'):
+            self.robot1_name = rospy.get_param('~robot1_name')
+            self.robot2_name = rospy.get_param('~robot2_name')
+        else:
+            rospy.logerr(f"The name of the robots is not set in {rospy.get_name()}")
+            sys.exit(0)
+
         # Initialize data stream
         self.trajObjPlan = np.empty(0)
         self.trajRobotPlan = np.empty(0)
@@ -48,8 +56,8 @@ class PlanInterpWithTO:
         listener = tf2_ros.TransformListener(self.IK_listen_buff)
 
         # start punlishers
-        self.new_Yangtraj_publisher = rospy.Publisher(NEW_TRAJ_Yang_TOPIC, Float64MultiArray, queue_size=1)
-        self.new_Yintraj_publisher = rospy.Publisher(NEW_TRAJ_Yin_TOPIC, Float64MultiArray, queue_size=1)
+        self.new_Yangtraj_publisher = rospy.Publisher(f'{self.robot1_name}/{NEW_TRAJ_TOPIC}', Float64MultiArray, queue_size=1)
+        self.new_Yintraj_publisher = rospy.Publisher(f'{self.robot2_name}/{NEW_TRAJ_TOPIC}', Float64MultiArray, queue_size=1)
         self.new_Objtraj_publisher = rospy.Publisher(NEW_TRAJ_OBJ_TOPIC, Float64MultiArray, queue_size=1)
 
         time.sleep(2.0) # wait for initialisation to complete
@@ -130,10 +138,10 @@ class PlanInterpWithTO:
     def solveTO(self):
 
         # get object and robot state from pybullet
-        basePosYang, baseAttYang, endPosYang, endAttYang = self.readInitials("yang")
+        basePosYang, baseAttYang, endPosYang, endAttYang = self.readInitials(self.robot1_name)
         print(endPosYang, endAttYang)
 
-        basePosYin, baseAttYin, endPosYin, endAttYin = self.readInitials("yin")
+        basePosYin, baseAttYin, endPosYin, endAttYin = self.readInitials(self.robot2_name)
         print(endPosYin, endAttYin)
 
         # get initial guess
@@ -153,16 +161,17 @@ class PlanInterpWithTO:
         # get bounds of variables and constraints
         if ori_representation == "euler":
             # euler representation initialization #
-            initObjPos = np.array([0.0, 0.0, 0.6, 0, 0, 0])
-            finObjPos = np.array([0., 0.0, 0.75, 0, 0, 0])
+            initObjPos = np.array([0.0, 0.15, 0.2, 0, 0, 0])
+            finObjPos = np.array([0., 0.0, 0.60, 0, 0, 0])
             maxObjPos = np.array([2, 2, 2, np.pi, np.pi, np.pi])
-            minObjPos = np.array([-2, -2, 0.6, -np.pi, -np.pi, -np.pi])
+            minObjPos = np.array([-2, -2, 0.15, -np.pi, -np.pi, -np.pi])
         elif ori_representation == "quaternion":
             # quaternion representation initialization #
             initObjPos = np.array([0, 0, 0.6, 0, 0, 0, 1])
             finObjPos = np.array([0, 0, 0, 0, 0, 0, 1])
             maxObjPos = np.array([2, 2, 2, np.pi, np.pi, np.pi])
             minObjPos = np.array([-2, -2, 0.6, -np.pi, -np.pi, -np.pi])
+
 
         initObjVel = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         finObjVel = np.array([0., 0., 0., 0., 0., 0.])
@@ -172,12 +181,12 @@ class PlanInterpWithTO:
         slackObjVel = np.array([0.001, 0.001, 0.001, 0.001, 0.001, 0.001])
 
         initArmPos1 = np.array([endPosYang[0], endPosYang[1], endPosYang[2], endAttYang[0], endAttYang[1], endAttYang[2]])
-        minArmPos1 = np.array([-1.0, -1.0, 0.2, -np.pi * 2, -np.pi * 2, -np.pi * 2])
-        maxArmPos1 = np.array([1.0, 1.0, 1.0, np.pi * 2, np.pi * 2, np.pi * 2])
+        minArmPos1 = np.array([-1.0, -1.0, 0.2, -np.pi, -np.pi, -np.pi])
+        maxArmPos1 = np.array([1.0, 1.0, 1.0, np.pi, np.pi, np.pi])
 
         initArmPos2 = np.array([endPosYin[0], endPosYin[1], endPosYin[2], endAttYin[0], endAttYin[1], endAttYin[2]])
-        minArmPos2 = np.array([-1.0, -1.0, 0.2, -np.pi * 2, -np.pi * 2, -np.pi * 2])
-        maxArmPos2 = np.array([1.0, 1.0, 1.0, np.pi * 2, np.pi * 2, np.pi * 2])
+        minArmPos2 = np.array([-1.0, -1.0, 0.2, -np.pi, -np.pi, -np.pi])
+        maxArmPos2 = np.array([1.0, 1.0, 1.0, np.pi, np.pi, np.pi])
 
         lbx, ubx, lbg, ubg, cf, gf = self.HybOpt_DAC.buildBounds(initObjPos, finObjPos, minObjPos, maxObjPos, slackObjPos,
                                                                  initObjVel, finObjVel, maxObjVel, slackObjVel,
@@ -204,41 +213,22 @@ class PlanInterpWithTO:
         rospy.loginfo(f"{self.name}: Reading for /tf topic the position and orientation of the robot")
         # Read the position and orientation of the robot from the /tf topic
 
-        if robot_name == "yang":
-            # trans = IK_listen_buff.lookup_transform(WORLD_FRAME_ID, f"{robot_name}/{ROBOT_BASE_ID}", rospy.Time())
-            trans = self.IK_listen_buff.lookup_transform(WORLD_FRAME_ID, f"{robot_name}/{ROBOT_BASE_ID}", rospy.Time())
-            # replaces base_position = config['base_position']
-            base_position = [trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z]
-            base_orient_quat = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z,
-                                trans.transform.rotation.w]
-            base_orient_euler = R.from_quat(base_orient_quat).as_euler('ZYX')
+        # trans = IK_listen_buff.lookup_transform(WORLD_FRAME_ID, f"{robot_name}/{ROBOT_BASE_ID}", rospy.Time())
+        trans = self.IK_listen_buff.lookup_transform(WORLD_FRAME_ID, f"{robot_name}/{ROBOT_BASE_ID}", rospy.Time())
+        # replaces base_position = config['base_position']
+        base_position = [trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z]
+        base_orient_quat = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z,
+                            trans.transform.rotation.w]
+        base_orient_euler = R.from_quat(base_orient_quat).as_euler('ZYX')
 
-            trans = self.IK_listen_buff.lookup_transform(WORLD_FRAME_ID, f"{robot_name}/{END_EFFECTOR_FRAME_ID}", rospy.Time())
-            # replaces base_position = config['base_position']
-            end_position = [trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z]
-            # replaces: base_orient_eulerXYZ = config['base_orient_eulerXYZ']
-            end_orient_quat = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z,
-                                trans.transform.rotation.w]
-            end_orient_euler = R.from_quat(end_orient_quat).as_euler('ZYX')
-            # base_orient_eulerXYZ = config['base_orient_eulerXYZ']
-
-        elif robot_name == "yin":
-            trans = self.IK_listen_buff.lookup_transform(WORLD_FRAME_ID, f"{robot_name}/{ROBOT_BASE_ID}", rospy.Time())
-            # replaces base_position = config['base_position']
-            base_position = [trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z]
-            # replaces: base_orient_eulerXYZ = config['base_orient_eulerXYZ']
-            base_orient_quat = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z,
-                                trans.transform.rotation.w]
-            base_orient_euler = R.from_quat(base_orient_quat).as_euler('ZYX')
-
-            trans = self.IK_listen_buff.lookup_transform(WORLD_FRAME_ID, f"{robot_name}/{END_EFFECTOR_FRAME_ID}",
-                                                         rospy.Time())
-            # replaces base_position = config['base_position']
-            end_position = [trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z]
-            # replaces: base_orient_eulerXYZ = config['base_orient_eulerXYZ']
-            end_orient_quat = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z,
-                               trans.transform.rotation.w]
-            end_orient_euler = R.from_quat(end_orient_quat).as_euler('ZYX')
+        trans = self.IK_listen_buff.lookup_transform(WORLD_FRAME_ID, f"{robot_name}/{END_EFFECTOR_FRAME_ID}", rospy.Time())
+        # replaces base_position = config['base_position']
+        end_position = [trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z]
+        # replaces: base_orient_eulerXYZ = config['base_orient_eulerXYZ']
+        end_orient_quat = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z,
+                            trans.transform.rotation.w]
+        end_orient_euler = R.from_quat(end_orient_quat).as_euler('ZYX')
+        # base_orient_eulerXYZ = config['base_orient_eulerXYZ']
 
         return base_position, base_orient_euler, end_position, end_orient_euler
 
