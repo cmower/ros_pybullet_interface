97864364d17170d7ec76415b9cc29ef9ce41643e
Chris E. Mower <chris.mower@ed.ac.uk>
2022-02-17 13:54:03 +0000

    Implement base class for ROS node

---
+++ ros_pybullet_interface/src/ros_pybullet_interface/ros_node.py
@@ -0,0 +1,54 @@
+import rospy
+
+"""
+
+ROS API
+
+The reason for this class is to minimize effort when parsing the
+ros_pybullet_interface to ROS2.
+
+"""
+
+class RosNode:
+
+    def init_node(self, *args, **kwargs):
+        rospy.init_node(*args, **kwargs)
+
+    def set_param(self, *args, **kwargs):
+        return rospy.set_param(*args, **kwargs)
+
+    def get_param(self, *args, **kwargs):
+        return rospy.get_param(*args, **kwargs)
+
+    def Publisher(self,  *args, **kwargs):
+        return rospy.Publisher(*args, **kwargs)
+
+    def Subscriber(self, *args, **kwargs):
+        return rospy.Subscriber(*args, **kwargs)
+
+    def Service(self, *args, **kwargs):
+        return rospy.Service(*args, **kwargs)
+
+    def Timer(self, *args, **kwargs):
+        return rospy.Timer(*args, **kwargs)
+
+    def Duration(self, *args, **kwargs):
+        return rospy.Duration(*args, **kwargs)
+
+    def logdebug(self, *args, **kwargs):
+        rospy.logdebug(*args, **kwargs)
+
+    def loginfo(self, *args, **kwargs):
+        rospy.loginfo(*args, **kwargs)
+
+    def logwarn(self, *args, **kwargs):
+        rospy.logwarn(*args, **kwargs)
+
+    def logerr(self, *args, **kwargs):
+        rospy.logerr(*args, **kwargs)
+
+    def logfatal(self, *args, **kwargs):
+        rospy.logfatal(*args, **kwargs)
+
+    def spin(self):
+        rospy.spin()
