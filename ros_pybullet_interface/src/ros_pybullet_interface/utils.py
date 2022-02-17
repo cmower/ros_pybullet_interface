27ed5d771ef134a0560c81f8758dcd693d1d3936
Chris E. Mower <chris.mower@ed.ac.uk>
2022-02-17 11:11:44 +0000

    Add UniqueDict class in new utils.py script

---
+++ ros_pybullet_interface/src/ros_pybullet_interface/utils.py
@@ -0,0 +1,8 @@
+
+class UniqueDict(dict):
+
+    def __setitem__(self, key, value):
+        if key not in self:
+            dict.__setitem__(self, key, value)
+        else:
+            raise KeyError(f"Key '{key}' already exists!")
