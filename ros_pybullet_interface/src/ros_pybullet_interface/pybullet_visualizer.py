d3e9a966ab309cef4ea6bd60360f9d3d03e76cb5
Chris Mower <chris.mower@ed.ac.uk>
2022-02-20 02:22:39 +0000

    Make visualizer configurable

--- ros_pybullet_interface/src/ros_pybullet_interface/pybullet_visualizer.py
+++ ros_pybullet_interface/src/ros_pybullet_interface/pybullet_visualizer.py
@@ -10,6 +10,11 @@ class PybulletVisualizer:
         'cameraTargetPosition': [0.0]*3,
     }
 
+    configure_debug_visualizer_default = dict(
+        flag='COV_ENABLE_GUI',
+        enable=0,
+    )
+
     def __init__(self, pb, node):
 
         # Set pybullet instance and ROS node
@@ -21,10 +26,11 @@ class PybulletVisualizer:
         self.config = load_config(config_filename) if config_filename else {}
 
         # Initialize visualizer
-        self.pb.configureDebugVisualizer(
-            flag=self.pb.COV_ENABLE_GUI,
-            enable=0,
-        )
+        configure_debug_visualizer_input = self.config.get('configureDebugVisualizer', self.configure_debug_visualizer_default)
+        if 'flag' in configure_debug_visualizer_input.keys():
+            flag = ['self.pb.' + f for f in configure_debug_visualizer_input['flag'].split('|')]
+            configure_debug_visualizer_input['flag'] = eval('|'.join(flag))
+        self.pb.configureDebugVisualizer(**configure_debug_visualizer_input)
         self.camera_config = {
             key: self.config.get(key, self.reset_debug_visualizer_camera_default[key])
             for key in self.reset_debug_visualizer_camera_default.keys()
