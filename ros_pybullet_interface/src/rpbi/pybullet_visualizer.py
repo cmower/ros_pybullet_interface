95c117e299739a5c9098541262ede9f0f2251ac9
Chris Mower <chris.mower@ed.ac.uk>
2022-03-22 18:35:38 +0000

    property'ify config variables

--- ros_pybullet_interface/src/rpbi/pybullet_visualizer.py
+++ ros_pybullet_interface/src/rpbi/pybullet_visualizer.py
@@ -25,16 +25,10 @@ class PybulletVisualizer:
         self.node = node
 
         # Initialize visualizer
-        configure_debug_visualizer_input = self.node.config.get('configureDebugVisualizer', self.configure_debug_visualizer_default)
-        if 'flag' in configure_debug_visualizer_input.keys():
-            flag = ['self.pb.' + f for f in configure_debug_visualizer_input['flag'].split('|')]
-            configure_debug_visualizer_input['flag'] = eval('|'.join(flag))
-        self.pb.configureDebugVisualizer(**configure_debug_visualizer_input)
-        self.camera_config = {
-            key: self.config.get(key, self.reset_debug_visualizer_camera_default[key])
-            for key in self.reset_debug_visualizer_camera_default.keys()
-        }
-        self.pb.resetDebugVisualizerCamera(**self.camera_config)
+        self.pb.configureDebugVisualizer(**self.configure_debug_visualizer)
+
+        # Initialize camera pose
+        self.pb.resetDebugVisualizerCamera(**self.reset_debug_visualizer_camera)
 
         # Setup subcscriber
         self.node.Subscriber('rpbi/reset_debug_visualizer_camera', ResetDebugVisualizerCamera, self.callback)
@@ -44,15 +38,71 @@ class PybulletVisualizer:
 
         self.node.loginfo('initialized Pybullet visualizer')
 
+    @property
+    def configure_debug_visualizer(self):
+
+        # Get from config
+        configure_debug_visualizer = self.node.config.get('configureDebugVisualizer', {})
+        for key, default_value in self.configure_debug_visualizer_default.items():
+            if key not in configure_debug_visualizer:
+                configure_debug_visualizer[key] = default_value
+
+        # Handle flags
+        if 'flag' in configure_debug_visualizer:
+
+            flag_success = True
+
+            if isinstance(configure_debug_visualizer['flag'], int):
+                pass
+            elif isinstance(configure_debug_visualizer['flag'], str):
+                flag = ['self.pb.' + f for f in configure_debug_visualizer['flag'].split('|')]
+                configure_debug_visualizer['flag'] = eval('|'.join(flag))
+            elif isinstance(configure_debug_visualizer['flag'], list):
+                if all(isinstance(f, (str, int)) for f in configure_debug_visualizer['flag']):
+
+                    # Set initial flag
+                    def to_int(f):
+                        if isinstance(f, str):
+                            return eval('self.pb.' + f)
+                        else:
+                            return f
+
+                    flag = to_int(configure_debug_visualizer['flag'][0])
+
+                    # Iterate over remaining flags
+                    for f in configure_debug_visualizer['flag'][1:]:
+                        flag |= to_int(f)
+
+                    # Set flag
+                    configure_debug_visualizer['flag'] = eval('|'.join(flag))
+
+                else:
+                    flag_success = False
+            else:
+                flag_success = False
+
+            if not flag_success:
+                raise TypeError("the type used for the flag argument in configureDebugVisualizer config is not recognized")
+
+        return configure_debug_visualizer
+
+    @property
+    def reset_debug_visualizer_camera(self):
+        return self.node.config.get('resetDebugVisualizerCamera', self.reset_debug_visualizer_camera_default)
+
+    @reset_debug_visualizer_camera.setter
+    def reset_debug_visualizer_camera(self, msg):
+        assert isinstance(msg, ResetDebugVisualizerCamera), "reset_debug_visualizer_camera"
+        if 'resetDebugVisualizerCamera' not in self.node.config: self.node.config['resetDebugVisualizerCamera'] = {}
+        for key in self.reset_debug_visualizer_camera_default.keys():
+            self.node.config['resetDebugVisualizerCamera'][key] = getattr(msg, key)
+
     def service_get_debug_visualizer_camera(self, req):
         resp = GetDebugVisualizerCameraResponse()
-        for key, value in self.camera_config.items(): setattr(resp, key, value)
+        for key, value in self.reset_debug_visualizer_camera.items(): setattr(resp, key, value)
         return resp
 
     def callback(self, msg):
         """Callback for subscriber listening for visualizer pose updates."""
-        self.camera_config = {
-            key: getattr(msg, key, self.reset_debug_visualizer_camera_default[key])
-            for key in self.reset_debug_visualizer_camera.keys()
-        }
-        self.pb.resetDebugVisualizerCamera(**self.camera_config)
+        self.reset_debug_visualizer_camera = msg
+        self.pb.resetDebugVisualizerCamera(**self.reset_debug_visualizer_camera)
