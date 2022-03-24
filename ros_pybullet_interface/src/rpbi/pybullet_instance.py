286ee3fa2a51ba08c6d94882daf61f95b83e4291
Chris Mower <chris.mower@ed.ac.uk>
2022-03-24 15:38:13 +0000

    Expose pybullet.connect method to user through config

--- ros_pybullet_interface/src/rpbi/pybullet_instance.py
+++ ros_pybullet_interface/src/rpbi/pybullet_instance.py
@@ -14,10 +14,11 @@ class PybulletInstance:
         self.node = node
 
         # Setup variables
+        self.manual_step_timer = None
         self.is_active = False
 
         # Connect to pybullet
-        self.client_id = self.pb.connect(self.pb.GUI)
+        self.client_id = self.pb.connect(self.connection_mode, **self.connect_kwargs)
         if self.client_id == -1:
             raise RuntimeError('Unable to connect to Pybullet!')
         self.node.loginfo(f'connected to Pybullet with client id {self.client_id}')
@@ -34,6 +35,14 @@ class PybulletInstance:
         self.dt = self.timeStep
         self.pb.setTimeStep(self.dt)
 
+        # Setup start/stop methods
+        if self.step_pybullet_manually:
+            self.start = self.start_manual
+            self.stop = self.stop_manual
+        else:
+            self.start = self.start_real_time_simulation
+            self.stop = self.stop_real_time_simulation
+
         # Setup services
         self.node.Service('rpbi/start', Trigger, partial(self._service, handle=self.start))
         self.node.Service('rpbi/step', Trigger, partial(self._service, handle=self.step))
@@ -43,6 +52,25 @@ class PybulletInstance:
         self.status_publisher = StatusPublisher(self)
 
         self.node.loginfo('initialized Pybullet instance')
+
+    @property
+    def connection_mode(self):
+        connect = self.node.config.copy().get('connect', {})
+        connection_mode = connect.get('connection_mode', self.pb.GUI)
+        if isinstance(connection_mode, int):
+            return connection_mode
+        elif isinstance(connection_mode, str):
+            return getattr(self.pb, connection_mode)
+        else:
+            raise ValueError("did not recognize given connection mode")
+
+    @property
+    def connect_kwargs(self):
+        connect = self.node.config.get('connect', {})  # use copy to prevent connection_mode from being removed
+        connect_ = connect.copy()
+        if 'connection_mode' in connect:
+            del connect_['connection_mode']
+        return connect_
 
     @property
     def start_pybullet_after_initialization(self):
@@ -56,7 +84,50 @@ class PybulletInstance:
     def timeStep(self):
         return self.node.config.get('timeStep', 0.02)
 
-    def start(self):
+    @property
+    def step_pybullet_manually(self):
+        if self.connection_mode == self.pb.DIRECT:
+            # DIRECT requires user to step manually, see:
+            # https://pybullet.org/Bullet/phpBB3/viewtopic.php?t=13064
+            return True
+        else:
+            return self.node.config.get('step_pybullet_manually', False)
+
+
+    def start_manual(self):
+        """Start Pybullet"""
+        if not self.is_active:
+            self.manual_step_timer = self.node.Timer(self.node.Duration(self.timeStep), self._step_manual)
+            self.is_active = True
+            success = True
+            message = 'started Pybullet'
+        else:
+            success = False
+            message = 'attempted to start Pybullet, but it already active!'
+            self.node.logerr(message)
+        return success, message
+
+
+    def _step_manual(self, event):
+        self.pb.stepSimulation()
+
+
+    def stop_manual(self):
+        """Stop Pybullet"""
+        if self.is_active:
+            self.manual_step_timer.shutdown()
+            self.manual_step_timer = None
+            self.is_active = False
+            success = True
+            message = 'stopped Pybullet'
+        else:
+            success = False
+            message = 'attempted to stop Pybullet, but it is not running!'
+            self.node.logerr(messsage)
+        return success, message
+
+
+    def start_real_time_simulation(self):
         """Start Pybullet"""
         if not self.is_active:
             self.pb.setRealTimeSimulation(1)
@@ -70,7 +141,7 @@ class PybulletInstance:
         return success, message
 
 
-    def stop(self):
+    def stop_real_time_simulation(self):
         """Stop Pybullet"""
         if self.is_active:
             self.pb.setRealTimeSimulation(0)
