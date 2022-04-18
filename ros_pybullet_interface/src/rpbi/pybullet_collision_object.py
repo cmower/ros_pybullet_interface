179a421b36c571728a404798cc463bec5306846d
Chris Mower <chris.mower@ed.ac.uk>
2022-04-18 14:28:57 +0100

    Update collision object pose

--- ros_pybullet_interface/src/rpbi/pybullet_collision_object.py
+++ ros_pybullet_interface/src/rpbi/pybullet_collision_object.py
@@ -12,33 +12,20 @@ class PybulletCollisionObject(PybulletObject):
         self.base_visual_shape_index = self.create_visual_shape(self.createVisualShape)
         self.base_collision_shape_index = self.create_collision_shape(self.createCollisionShape)
 
+        # Create body unique id
+        self.body_unique_id = self.pb.createMultiBody(
+            baseMass=0.0,
+            baseVisualShapeIndex=self.base_visual_shape_index,
+            baseCollisionShapeIndex=self.base_collision_shape_index,
+        )
+        
+        # Set dynamics
+        self.change_dynamics(self.changeDynamics)        
+
         # Setup object pose handler
         self.pose = PybulletObjectPose(self)
-
-        if self.pose.is_static:
-            self.pose.get_base_from_tf()
-            pos, rot = self.pose.get()
-            self.body_unique_id = self.pb.createMultiBody(
-                baseMass=0.0,
-                baseVisualShapeIndex=self.base_visual_shape_index,
-                baseCollisionShapeIndex=self.base_collision_shape_index,
-                basePosition=pos,
-                baseOrientation=rot
-            )
-            if self.pose.broadcast_tf:
-                self.pose.start_pose_broadcaster()
-        else:
-            self.body_unique_id = self.pb.createMultiBody(
-                baseMass=0.0,
-                baseVisualShapeIndex=self.base_visual_shape_index,
-                baseCollisionShapeIndex=self.base_collision_shape_index
-            )
-            self.pose.start_resetter()
-            if self.pose.broadcast_tf:
-                self.node.logwarn("can not broadcast a non-static pose")
-
-        # Set dynamics
-        self.change_dynamics(self.changeDynamics)
+        if self.pose.tf_specified():
+            self.pose.start_reset_pose()
 
     @property
     def createVisualShape(self):
