2cee4172430ff522c51a5ec8871a40290c349dd6
Chris Mower <chris.mower@ed.ac.uk>
2022-05-01 15:46:11 +0100

    Add button that sends robot to initial joint configuration

--- ros_pybullet_interface/scripts/rpbi_controls_node.py
+++ ros_pybullet_interface/scripts/rpbi_controls_node.py
@@ -3,6 +3,8 @@ import tkinter
 from std_msgs.msg import Int64
 from rpbi.ros_node import RosNode
 from std_srvs.srv import Trigger, TriggerResponse
+from sensor_msgs.msg import JointState
+from ros_pybullet_interface.srv import ResetJointState
 from custom_ros_tools.ros_comm import get_srv_handler
 
 class Window(tkinter.Frame):
@@ -19,10 +21,12 @@ class Window(tkinter.Frame):
         self.start_button = tkinter.Button(self, text="Start", command=self.start_button_handler)
         self.step_button = tkinter.Button(self, text="Step", command=self.step_button_handler)
         self.stop_button = tkinter.Button(self, text="Stop", command=self.stop_button_handler)
+        self.send_button = tkinter.Button(self, text='Send', command=self.send_button_handler)
 
         self.start_button.place(x=0, y=0)
         self.step_button.place(x=0, y=100)
         self.stop_button.place(x=0, y=200)
+        self.send_button.place(x=100, y=0)
 
         self.text_box = tkinter.Text(self, height=100, width=200)
         self.text_box.place(x=100, y=300)
@@ -40,15 +44,25 @@ class Window(tkinter.Frame):
     def stop_button_handler(self):
         self.node.stop_button_handler()
 
+    def send_button_handler(self):
+        self.node.send_robot()
+
 class Node(RosNode):
 
     def __init__(self):
         super().__init__('rpbi_controls_node')
         self.on_shutdown(self.close)
+        self.config = self.get_param('~config', {}).get('controls', {})
         self.Subscriber('rpbi/status', Int64, self.status_callback)
         self.start_button_handler = get_srv_handler('rpbi/start', Trigger, persistent=True)
         self.step_button_handler = get_srv_handler('rpbi/step', Trigger, persistent=True)
         self.stop_button_handler = get_srv_handler('rpbi/stop', Trigger, persistent=True)
+
+        robot_name = self.config.get('robot_name', None)
+        if robot_name:
+            self.send_button_handler = get_srv_handler(f'rpbi/{robot_name}/move_to_initial_joint_state', ResetJointState, persistent=True)
+        else:
+            self.send_button_handler = None            
         self.root = tkinter.Tk()
         self.app = Window(self, self.root)
 
@@ -56,6 +70,10 @@ class Node(RosNode):
         if not hasattr(self, 'app'): return
         self.app.text_box.delete("1.0","end")
         self.app.text_box.insert(tkinter.END, f"Status: {msg.data}")
+
+    def send_robot(self, state=None):
+        if self.send_button_handler:
+            self.send_button_handler(JointState(), 6.0)
 
     def close(self):
         self.root.quit()
